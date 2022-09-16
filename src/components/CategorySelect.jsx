086ceb57f2d77c3b898e35dd2b08868860398840
Ibsy Matute <ibsy@rocketmail.com>
2022-09-15 21:53:40 -0500

    added categories from data to category dropdown

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function CategorySelect() {
+
+    const [suggestions, setSuggestions] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/suggestions")
+            .then(response => response.json())
+            .then(data => {
+                let suggestionData = data
+                setSuggestions(suggestionData)
+            })
+    }, [])
+
+    let categories = []
+
+    {suggestions.map((suggestion, idx) => {
+        if (!categories.includes(suggestion.category)){
+            categories.push(suggestion.category)
+        }
+        return (
+            categories.sort()
+        )
+    })}
+
     return (
         <>
-            {/* <div className="col-4"> */}
                 <select className="form-select" aria-label="Default select example">
                     <option defaultValue>Random</option>
-                    <option value="1">Outdoor Recreational</option>
-                    <option value="2">Educational</option>
-                    <option value="3">Entertainment</option>
+                    {categories.map((category, idx) => {
+                        return (
+                            <option value={idx + 1} key={idx}>{category}</option>
+                        )}
+                    )}
                 </select>
-            {/* </div> */}
         </>
     )
 }
